edc0d360814e343cb386e09475b03241c0db497d
AtariKing <157655439+AtariKing@users.noreply.github.com>
2024-04-18 00:47:47 +0000

    lots o' circles

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -32,11 +32,10 @@ var init = function (window) {
         }
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++) {
         drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
+        }
+        
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -48,18 +47,13 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0])
-            physikz.updatePosition(circles[1])
-            physikz.updatePosition(circles[2])
-            physikz.updatePosition(circles[3])
-            physikz.updatePosition(circles[4])
+            for (var i = 0; i < circles.length; i++) {
+            physikz.updatePosition(circles[i])
+        }
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0]);
-            game.checkCirclePosition(circles[1]);
-            game.checkCirclePosition(circles[2]);
-            game.checkCirclePosition(circles[3]);
-            game.checkCirclePosition(circles[4]);
-
+            for (var i = 0; i < circles.length; i++) {
+            game.checkCirclePosition(circles[i]);
+        }
             // TODO 9 : Iterate over the array
            
             
@@ -82,11 +76,11 @@ var init = function (window) {
                 if (circle.x > canvas.width) {
                     circle.x = 0
                 }else if (circle.x < 0) {
-                    circle.x = 0
+                    circle.x = canvas.width
                 }else if (circle.y > canvas.height) {
                     circle.y = 0
                 }else if (circle.y < 0) {
-                    circle.y = 0
+                    circle.y = canvas.height
                 }
             }
 
