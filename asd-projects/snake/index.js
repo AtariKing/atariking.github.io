a23460f93d9cacdfebb28b0fe5cadfe582c91a1e
AtariKing <157655439+AtariKing@users.noreply.github.com>
2024-12-12 23:48:35 +0000

    working on Snake todo 4a

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,7 +11,9 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
-
+var snake = {
+  
+}
 // Constant Variables
 var ROWS = 20;
 var COLUMNS = 20;
